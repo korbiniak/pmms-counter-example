c52dc3951f1cf63b5191619cc4d6433e90cca82d
Franciszek Malinka <franciszek.malinka@gmail.com>
2024-06-05 22:25:08 +0200

    Setup benchmarking library

---
+++ benchmark/benchmark_pmms.cpp
@@ -0,0 +1,17 @@
+#include <benchmark/benchmark.h>
+#include "allocation.h"
+#include "pmms.h"
+#include "valuation.h"
+
+static void BM_VectorPushBack(benchmark::State& state) {
+    for (auto _ : state) {
+        std::vector<int> v;
+        for (int i = 0; i < state.range(0); ++i) {
+            v.push_back(i);
+        }
+    }
+}
+BENCHMARK(BM_VectorPushBack)->Range(8, 8<<10);
+
+BENCHMARK_MAIN();
+ 
